b54b51de9914b74460b7e8509f835ea27847629f
liveforone <yc4852@gmail.com>
2023-07-07 16:49:29 +0900

    Add member service validator

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/member/service/validator/MemberServiceValidator.kt
@@ -0,0 +1,19 @@
+package intelligent_booking.intelligent_booking.member.service.validator
+
+import intelligent_booking.intelligent_booking.exception.exception.MemberException
+import intelligent_booking.intelligent_booking.exception.message.MemberExceptionMessage
+import intelligent_booking.intelligent_booking.member.repository.MemberRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class MemberServiceValidator @Autowired constructor(
+    private val memberRepository: MemberRepository
+) {
+
+    fun validateDuplicateEmail(email: String) {
+        if (memberRepository.findIdByEmailNullableForValidate(email) != null) {
+            throw MemberException(MemberExceptionMessage.DUPLICATE_EMAIL)
+        }
+    }
+}
