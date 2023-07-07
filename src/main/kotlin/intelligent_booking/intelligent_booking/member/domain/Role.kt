70e01c4e1b9ff1950c4f591a46408733f83a6f1c
liveforone <yc4852@gmail.com>
2023-07-07 16:31:14 +0900

    Add role.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/member/domain/Role.kt
@@ -0,0 +1,7 @@
+package intelligent_booking.intelligent_booking.member.domain
+
+enum class Role(val auth:String) {
+    MEMBER("ROLE_MEMBER"),
+    PRESIDENT("ROLE_PRESIDENT"),
+    ADMIN("ROLE_ADMIN")
+}
