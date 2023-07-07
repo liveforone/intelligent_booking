c710f9c4bb74a5d470fb438f4b9e84e13d2b90ef
liveforone <yc4852@gmail.com>
2023-07-07 16:49:20 +0900

    Add member domain test.

---
+++ src/test/kotlin/intelligent_booking/intelligent_booking/member/domain/MemberTest.kt
@@ -0,0 +1,38 @@
+package intelligent_booking.intelligent_booking.member.domain
+
+import intelligent_booking.intelligent_booking.member.domain.util.PasswordUtil
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class MemberTest {
+
+    @Test
+    fun updateEmailTest() {
+        //given
+        val email = "test1234@gmail.com"
+        val pw = "1234"
+        val member = Member.create(email, pw, Role.MEMBER)
+
+        //when
+        val newEmail = "test_update_email@gmail.com"
+        member.updateEmail(newEmail)
+
+        //then
+        Assertions.assertThat(member.email).isEqualTo(newEmail)
+    }
+
+    @Test
+    fun updatePwTest() {
+        //given
+        val email = "test1234@gmail.com"
+        val pw = "1234"
+        val member = Member.create(email, pw, Role.MEMBER)
+
+        //when
+        val newPw = "1111"
+        member.updatePw(newPw, pw)
+
+        //then
+        Assertions.assertThat(PasswordUtil.isMatchPassword(newPw, member.pw)).isTrue()
+    }
+}
