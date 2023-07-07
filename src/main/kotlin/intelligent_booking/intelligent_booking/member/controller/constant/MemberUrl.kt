677d638f7a54a4291cbed51caee2d47376d8d3a1
liveforone <yc4852@gmail.com>
2023-07-07 16:50:28 +0900

    Add member url.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/member/controller/constant/MemberUrl.kt
@@ -0,0 +1,12 @@
+package intelligent_booking.intelligent_booking.member.controller.constant
+
+object MemberUrl {
+    const val SIGNUP_MEMBER = "/member/signup"
+    const val SIGNUP_PRESIDENT = "/member/signup/president"
+    const val LOGIN = "/member/login"
+    const val INFO = "/member/info"
+    const val UPDATE_EMAIL = "/member/update/email"
+    const val UPDATE_PASSWORD = "/member/update/password"
+    const val WITHDRAW = "/member/withdraw"
+    const val PROHIBITION = "/prohibition"
+}
