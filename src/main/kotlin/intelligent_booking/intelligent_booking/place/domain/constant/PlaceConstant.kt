53f5e47f395bd195133653297082a124e9807b40
liveforone <yc4852@gmail.com>
2023-07-07 19:35:54 +0900

    Add place constant.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/place/domain/constant/PlaceConstant.kt
@@ -0,0 +1,6 @@
+package intelligent_booking.intelligent_booking.place.domain.constant
+
+object PlaceConstant {
+    const val MEMBER_COLUMN_NAME = "member_identity"
+    const val IDENTITY = "identity"
+}
