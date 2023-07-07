eed50593949d768b138a7d0ed0fd562a564423cd
liveforone <yc4852@gmail.com>
2023-07-07 16:34:25 +0900

    Add member controller advice.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/exception/controllerAdvice/MemberAndSecurityControllerAdvice.kt
@@ -0,0 +1,34 @@
+package intelligent_booking.intelligent_booking.exception.controllerAdvice
+
+import intelligent_booking.intelligent_booking.exception.exception.JwtCustomException
+import intelligent_booking.intelligent_booking.exception.exception.MemberException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class MemberAndSecurityControllerAdvice {
+
+    @ExceptionHandler(BadCredentialsException::class)
+    fun loginFailHandle(): ResponseEntity<*> {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body("로그인에 실패했습니다.")
+    }
+
+    @ExceptionHandler(MemberException::class)
+    fun memberExceptionHandle(memberException: MemberException): ResponseEntity<*> {
+        return ResponseEntity
+            .status(memberException.memberExceptionMessage.status)
+            .body(memberException.message)
+    }
+
+    @ExceptionHandler(JwtCustomException::class)
+    fun jwtCustomException(jwtCustomException: JwtCustomException): ResponseEntity<*> {
+        return ResponseEntity
+            .status(jwtCustomException.jwtExceptionMessage.status)
+            .body(jwtCustomException.message)
+    }
+}
