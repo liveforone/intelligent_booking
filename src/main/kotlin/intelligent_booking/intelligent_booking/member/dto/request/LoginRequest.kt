25baa17e636df004ed31c2fe56558482de1633e5
liveforone <yc4852@gmail.com>
2023-07-07 16:48:06 +0900

    Add request dto.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/member/dto/request/LoginRequest.kt
@@ -0,0 +1,10 @@
+package intelligent_booking.intelligent_booking.member.dto.request
+
+import jakarta.validation.constraints.NotBlank
+
+data class LoginRequest(
+    @field:NotBlank(message = "이메일을 입력하세요.")
+    var email:String?,
+    @field:NotBlank(message = "비밀번호를 입력하세요.")
+    var pw:String?
+)
