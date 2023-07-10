0b10a385c186c0fceac510b64f7b0629f423d788
liveforone <yc4852@gmail.com>
2023-07-10 18:22:16 +0900

    Add place url.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/place/controller/constant/PlaceUrl.kt
@@ -0,0 +1,12 @@
+package intelligent_booking.intelligent_booking.place.controller.constant
+
+object PlaceUrl {
+    const val DETAIL = "/place/detail/{uuid}"
+    const val MY_PLACE = "/place/me/{memberUUID}"
+    const val PLACE_HOME = "/place/home"
+    const val SEARCH_NAME = "/place/search/name"
+    const val SEARCH_ADDRESS = "/place/search/address"
+    const val CREATE_PLACE = "/place/create/{memberUUID}"
+    const val UPDATE_TEL = "/place/update/tel/{uuid}"
+    const val UPDATE_ADDRESS = "/place/update/address/{uuid}"
+}
