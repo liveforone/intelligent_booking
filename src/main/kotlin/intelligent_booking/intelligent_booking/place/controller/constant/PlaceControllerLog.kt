ffa60c26d78dbd70c3033179806d0fc93574e21b
liveforone <yc4852@gmail.com>
2023-07-10 18:22:37 +0900

    Add place controller log.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/place/controller/constant/PlaceControllerLog.kt
@@ -0,0 +1,7 @@
+package intelligent_booking.intelligent_booking.place.controller.constant
+
+enum class PlaceControllerLog(val log: String) {
+    CREATE_PLACE_SUCCESS("장소를 성공적으로 등록하였습니다."),
+    UPDATE_TEL_SUCCESS("전화번호를 성공적으로 변경하였습니다."),
+    UPDATE_ADDRESS_SUCCESS("주소를 성공적으로 변경하였습니다.")
+}
