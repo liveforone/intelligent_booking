6ce004811471b695e99d2da0d1f2a70a39de791a
liveforone <yc4852@gmail.com>
2023-07-10 18:22:22 +0900

    Add place param.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/place/controller/constant/PlaceParam.kt
@@ -0,0 +1,11 @@
+package intelligent_booking.intelligent_booking.place.controller.constant
+
+object PlaceParam {
+    const val UUID = "uuid"
+    const val MEMBER_UUID = "memberUUID"
+    const val LAST_ID = "lastId"
+    const val NAME = "name"
+    const val CITY = "city"
+    const val ROAD_NUM = "roadNum"
+    const val DETAIL = "detail"
+}
