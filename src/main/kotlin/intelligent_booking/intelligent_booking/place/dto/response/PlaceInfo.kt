71ae51f3796203e2d6cf0812499fac4dce3a9970
liveforone <yc4852@gmail.com>
2023-07-10 18:21:38 +0900

    Refactor id field change to uuid.

--- src/main/kotlin/intelligent_booking/intelligent_booking/place/dto/response/PlaceInfo.kt
+++ src/main/kotlin/intelligent_booking/intelligent_booking/place/dto/response/PlaceInfo.kt
@@ -1,9 +1,10 @@
 package intelligent_booking.intelligent_booking.place.dto.response
 
 import intelligent_booking.intelligent_booking.place.domain.Address
+import java.util.UUID
 
 data class PlaceInfo(
-    val id: Long?,
+    val uuid: UUID,
     val name: String,
     val tel: String,
     val address: Address
