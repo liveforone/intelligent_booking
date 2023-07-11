b9a6f2361e25665299db72aca2e46e9ac709fc3a
liveforone <yc4852@gmail.com>
2023-07-11 13:40:33 +0900

    Add suspend auth.

--- src/main/kotlin/intelligent_booking/intelligent_booking/member/domain/Role.kt
+++ src/main/kotlin/intelligent_booking/intelligent_booking/member/domain/Role.kt
@@ -3,5 +3,6 @@ package intelligent_booking.intelligent_booking.member.domain
 enum class Role(val auth:String) {
     MEMBER("ROLE_MEMBER"),
     PRESIDENT("ROLE_PRESIDENT"),
-    ADMIN("ROLE_ADMIN")
+    ADMIN("ROLE_ADMIN"),
+    SUSPEND("ROLE_SUSPEND")
 }
