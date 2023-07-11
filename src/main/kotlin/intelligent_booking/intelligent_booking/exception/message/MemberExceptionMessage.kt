fab5ad5a51111108486a7f36c5987ea197f94c45
liveforone <yc4852@gmail.com>
2023-07-11 13:41:57 +0900

    Add suspend member message.

--- src/main/kotlin/intelligent_booking/intelligent_booking/exception/message/MemberExceptionMessage.kt
+++ src/main/kotlin/intelligent_booking/intelligent_booking/exception/message/MemberExceptionMessage.kt
@@ -1,6 +1,7 @@
 package intelligent_booking.intelligent_booking.exception.message
 
 enum class MemberExceptionMessage(val status: Int, val message: String) {
+    SUSPEND_MEMBER(401, "정지된 계정입니다."),
     WRONG_PASSWORD(400, "비밀번호를 틀렸습니다."),
     MEMBER_IS_NULL(404, "회원이 존재하지 않습니다."),
     DUPLICATE_EMAIL(400, "중복되는 이메일이 존재합니다.")
