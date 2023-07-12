7a5a4eb2ccafe70f9abab74be644eb54b06b80a4
liveforone <yc4852@gmail.com>
2023-07-12 22:04:43 +0900

    Add timetable query constant.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/timetable/repository/constant/TimetableQuery.kt
@@ -0,0 +1,5 @@
+package intelligent_booking.intelligent_booking.timetable.repository.constant
+
+object TimetableQuery {
+    const val RESTORE_REMAINING_COUNT = "UPDATE Timetable t SET t.remainingCount = t.basicCount"
+}
