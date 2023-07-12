efd288531785f6e7133e70747a2d5e354f608160
liveforone <yc4852@gmail.com>
2023-07-12 22:02:11 +0900

    Add timetable constant.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/timetable/domain/constant/TimetableConstant.kt
@@ -0,0 +1,6 @@
+package intelligent_booking.intelligent_booking.timetable.domain.constant
+
+object TimetableConstant {
+    const val ONE_COUNT: Long = 1
+    const val SUBTRACT_LIMIT_COUNT: Long = 0
+}
