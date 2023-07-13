07311f3156a26794ac19f829b0dc4d027ddbdbfa
liveforone <yc4852@gmail.com>
2023-07-13 19:34:46 +0900

    Add reservation info dto.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/reservation/controller/response/ReservationResponse.kt
@@ -0,0 +1,15 @@
+package intelligent_booking.intelligent_booking.reservation.controller.response
+
+import intelligent_booking.intelligent_booking.reservation.dto.response.ReservationInfo
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
+object ReservationResponse {
+    private const val RESERVATION_SUCCESS = "성공적으로 예약하였습니다."
+    private const val CANCEL_RESERVATION_SUCCESS = "예약을 성공적으로 취소하였습니다."
+
+    fun reservationDetailSuccess(reservation: ReservationInfo) = ResponseEntity.ok(reservation)
+    fun memberReservationSuccess(reservations: List<ReservationInfo>) = ResponseEntity.ok(reservations)
+    fun reservationSuccess() = ResponseEntity.status(HttpStatus.CREATED).body(RESERVATION_SUCCESS)
+    fun cancelReservationSuccess() = ResponseEntity.ok(CANCEL_RESERVATION_SUCCESS)
+}
