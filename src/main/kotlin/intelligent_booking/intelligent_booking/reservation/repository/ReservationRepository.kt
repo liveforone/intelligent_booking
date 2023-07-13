6590ae30e617e45e763008b1b29566b895909830
liveforone <yc4852@gmail.com>
2023-07-13 20:42:50 +0900

    Add reservation bulk delete query method.

--- src/main/kotlin/intelligent_booking/intelligent_booking/reservation/repository/ReservationRepository.kt
+++ src/main/kotlin/intelligent_booking/intelligent_booking/reservation/repository/ReservationRepository.kt
@@ -1,6 +1,16 @@
 package intelligent_booking.intelligent_booking.reservation.repository
 
+import intelligent_booking.intelligent_booking.member.domain.Member
 import intelligent_booking.intelligent_booking.reservation.domain.Reservation
+import intelligent_booking.intelligent_booking.reservation.repository.constant.ReservationQueryParam
+import intelligent_booking.intelligent_booking.reservation.repository.constant.ReservationStaticQuery
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
-interface ReservationRepository : JpaRepository<Reservation, Long>, ReservationCustomRepository
+interface ReservationRepository : JpaRepository<Reservation, Long>, ReservationCustomRepository {
+    @Modifying(clearAutomatically = true)
+    @Query(ReservationStaticQuery.DELETE_BY_MEMBER)
+    fun deleteBulkByMember(@Param(ReservationQueryParam.MEMBER) member: Member)
+}
