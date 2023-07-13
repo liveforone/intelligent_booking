ed94294188d5c0182d03370368582376d2353e13
liveforone <yc4852@gmail.com>
2023-07-13 13:55:07 +0900

    Add timetable restore policy..

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/timetable/service/command/constant/TimetableRestorePolicy.kt
@@ -0,0 +1,6 @@
+package intelligent_booking.intelligent_booking.timetable.service.command.constant
+
+object TimetableRestorePolicy {
+    const val TIMETABLE_RESTORE_COUNT_POLICY = "0 59 23 * * *"
+    const val RESTORE_COUNT_LOG = "!!Timetable Count Restored!!"
+}
