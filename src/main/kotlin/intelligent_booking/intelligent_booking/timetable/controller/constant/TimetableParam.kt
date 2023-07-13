afd81e6840a63f8f9ea0385076687d444c6aff91
liveforone <yc4852@gmail.com>
2023-07-13 13:54:55 +0900

    Add timetable param.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/timetable/controller/constant/TimetableParam.kt
@@ -0,0 +1,7 @@
+package intelligent_booking.intelligent_booking.timetable.controller.constant
+
+object TimetableParam {
+    const val UUID = "uuid"
+    const val PLACE_UUID = "placeUUID"
+    const val LAST_UUID = "lastUUID"
+}
