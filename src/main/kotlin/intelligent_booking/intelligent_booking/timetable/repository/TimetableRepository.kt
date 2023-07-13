adad14c2dd4583fd9326aac50e6a359299c2f892
liveforone <yc4852@gmail.com>
2023-07-13 20:43:19 +0900

    Add timetable bulk delete query method.

--- src/main/kotlin/intelligent_booking/intelligent_booking/timetable/repository/TimetableRepository.kt
+++ src/main/kotlin/intelligent_booking/intelligent_booking/timetable/repository/TimetableRepository.kt
@@ -1,13 +1,21 @@
 package intelligent_booking.intelligent_booking.timetable.repository
 
+import intelligent_booking.intelligent_booking.place.domain.Place
 import intelligent_booking.intelligent_booking.timetable.domain.Timetable
-import intelligent_booking.intelligent_booking.timetable.repository.constant.TimetableQuery
+import intelligent_booking.intelligent_booking.timetable.repository.constant.TimetableStaticQuery
+import intelligent_booking.intelligent_booking.timetable.repository.constant.TimetableQueryParam
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.util.UUID
 
 interface TimetableRepository : JpaRepository<Timetable, Long>, TimetableCustomRepository {
     @Modifying(clearAutomatically = true)
-    @Query(TimetableQuery.RESTORE_REMAINING_COUNT)
+    @Query(TimetableStaticQuery.RESTORE_REMAINING_COUNT)
     fun restoreCount()
+
+    @Modifying(clearAutomatically = true)
+    @Query(TimetableStaticQuery.DELETE_BY_PLACE)
+    fun deleteBulkByPlace(@Param(TimetableQueryParam.PLACE) place: Place)
 }
