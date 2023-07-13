4bd23209fc06fec77f54a1b433df28e1d0bea498
liveforone <yc4852@gmail.com>
2023-07-13 13:55:25 +0900

    Add timetable controller log.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/timetable/controller/constant/TimetableControllerLog.kt
@@ -0,0 +1,7 @@
+package intelligent_booking.intelligent_booking.timetable.controller.constant
+
+enum class TimetableControllerLog(val log: String) {
+    CREATE_TIMETABLE_SUCCESS("타임테이블을 생성하였습니다."),
+    UPDATE_DESCRIPTION_SUCCESS("설명을 변경하였습니다."),
+    DELETE_TIMETABLE_SUCCESS("!!타임테이블을 삭제하였습니다!!")
+}
