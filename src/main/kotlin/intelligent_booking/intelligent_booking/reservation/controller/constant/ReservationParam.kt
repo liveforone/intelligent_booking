53e8904e5fa8f19077859ce9b0853645c602bb45
liveforone <yc4852@gmail.com>
2023-07-13 19:35:50 +0900

    Add reservation param.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/reservation/controller/constant/ReservationParam.kt
@@ -0,0 +1,7 @@
+package intelligent_booking.intelligent_booking.reservation.controller.constant
+
+object ReservationParam {
+    const val UUID = "uuid"
+    const val MEMBER_UUID = "memberUUID"
+    const val LAST_UUID = "lastUUID"
+}
