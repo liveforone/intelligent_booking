fee0d16d4a7d976d1dcd6554aa3d1acbd4f9f6e5
liveforone <yc4852@gmail.com>
2023-07-13 19:35:44 +0900

    Add reservation url.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/reservation/controller/constant/ReservationUrl.kt
@@ -0,0 +1,8 @@
+package intelligent_booking.intelligent_booking.reservation.controller.constant
+
+object ReservationUrl {
+    const val DETAIL = "/reservation/detail/{uuid}"
+    const val RESERVATION_BELONG_MEMBER = "/reservation/member/{memberUUID}"
+    const val RESERVATION = "/reservation/create"
+    const val CANCEL = "/reservation/cancel/{uuid}"
+}
