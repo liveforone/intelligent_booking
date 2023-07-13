cea143c21102cc09e0d3090201972c0ad2df348d
liveforone <yc4852@gmail.com>
2023-07-13 19:36:21 +0900

    Add reservation controller log.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/reservation/controller/constant/ReservationControllerLog.kt
@@ -0,0 +1,6 @@
+package intelligent_booking.intelligent_booking.reservation.controller.constant
+
+enum class ReservationControllerLog(val log: String) {
+    RESERVATION_SUCCESS("예약 성공"),
+    CANCEL_SUCCESS("예약 취소 성공")
+}
