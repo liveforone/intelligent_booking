469745741621f3efdbefb6220e67fc736a615aca
liveforone <yc4852@gmail.com>
2023-07-13 20:43:09 +0900

    Add timetable static query.

--- src/main/kotlin/intelligent_booking/intelligent_booking/timetable/repository/constant/TimetableStaticQuery.kt
+++ src/main/kotlin/intelligent_booking/intelligent_booking/timetable/repository/constant/TimetableStaticQuery.kt
@@ -1,5 +1,6 @@
 package intelligent_booking.intelligent_booking.timetable.repository.constant
 
-object TimetableQuery {
+object TimetableStaticQuery {
     const val RESTORE_REMAINING_COUNT = "UPDATE Timetable t SET t.remainingCount = t.basicCount"
+    const val DELETE_BY_PLACE = "DELETE from Timetable t where t.place = :place"
 }
