9646134891fb77d82edf5f236d26884d91142df6
liveforone <yc4852@gmail.com>
2023-07-14 17:51:21 +0900

    Add Review param.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/review/controller/constant/ReviewParam.kt
@@ -0,0 +1,8 @@
+package intelligent_booking.intelligent_booking.review.controller.constant
+
+object ReviewParam {
+    const val UUID = "uuid"
+    const val LAST_UUID = "lastUUID"
+    const val MEMBER_UUID = "memberUUID"
+    const val PLACE_UUID = "placeUUID"
+}
