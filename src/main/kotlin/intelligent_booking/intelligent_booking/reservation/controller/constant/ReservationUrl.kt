5a85790d704cc1c99e070ef9e2ce818c4b6b8856
liveforone <yc4852@gmail.com>
2023-07-14 14:23:02 +0900

    Add timetable reservation url.

--- src/main/kotlin/intelligent_booking/intelligent_booking/reservation/controller/constant/ReservationUrl.kt
+++ src/main/kotlin/intelligent_booking/intelligent_booking/reservation/controller/constant/ReservationUrl.kt
@@ -3,6 +3,7 @@ package intelligent_booking.intelligent_booking.reservation.controller.constant
 object ReservationUrl {
     const val DETAIL = "/reservation/detail/{uuid}"
     const val RESERVATION_BELONG_MEMBER = "/reservation/member/{memberUUID}"
+    const val RESERVATION_BELONG_TIMETABLE = "/reservation/timetable/{timetableUUID}"
     const val RESERVATION = "/reservation/create"
     const val CANCEL = "/reservation/cancel/{uuid}"
 }
