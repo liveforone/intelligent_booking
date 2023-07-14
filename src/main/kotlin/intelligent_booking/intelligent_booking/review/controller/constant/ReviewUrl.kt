4a13c11c3185dba50beab9981a10698deb38413e
liveforone <yc4852@gmail.com>
2023-07-14 17:51:32 +0900

    Add Review url.

---
+++ src/main/kotlin/intelligent_booking/intelligent_booking/review/controller/constant/ReviewUrl.kt
@@ -0,0 +1,8 @@
+package intelligent_booking.intelligent_booking.review.controller.constant
+
+object ReviewUrl {
+    const val DETAIL = "/review/detail/{uuid}"
+    const val MEMBER_REVIEWS = "/review/member/{memberUUID}"
+    const val PLACE_REVIEWS = "/review/place/{placeUUID}"
+    const val CREATE_REVIEW = "/review/create"
+}
